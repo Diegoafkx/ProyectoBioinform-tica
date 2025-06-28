585bfea8cdb89ed0537c7676202d90494c049455
diego arreaza <diego.arreaza@correo.unimet.edu.ve>
2025-06-28 19:39:16 -0400

    Update patronADN.java

--- src/Estructura_de_datos/patronADN.java
+++ src/Estructura_de_datos/patronADN.java
@@ -12,18 +12,3 @@ package Estructura_de_datos;
 public class patronADN {
     
 }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Estructura_de_datos;
-
-/**
- *
- * @author vyckh
- */
-public class patronADN {
-    
-}
->>>>>>> d31299a0c6942248c37f2039a513854424c2d4e3
