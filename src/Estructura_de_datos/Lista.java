39c5d7a8c4abbe881445a77ecf598049ba14ee1a
Diegoafkx <diego.arreaza@correo.unimet.edu.ve>
2025-06-28 18:28:49 -0400

    prueba

---
+++ src/Estructura_de_datos/Lista.java
@@ -0,0 +1,561 @@
+<<<<<<< HEAD
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estructura_de_datos;
+
+/**
+ *
+ * @author Diego Arreaza y Vyckhy Sarmiento
+ */
+public class Lista {
+    
+     //atributos
+    private Nodo pFirst;
+    private int size;
+    private Nodo pLast;
+    
+    //constructor
+    public Lista() {
+        this.pFirst = null;
+        this.size = 0;
+        this.pLast = null;
+    }
+    
+   
+    //getters y setters
+    public Nodo getpFirst() {
+        return pFirst;
+    }
+
+    public void setpFirst(Nodo pFirst) {
+        this.pFirst = pFirst;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Nodo getpLast() {
+        return pLast;
+    }
+
+    public void setpLast(Nodo pLast) {
+        this.pLast = pLast;
+    }
+  
+    //Es vacio
+    public boolean EsVacio(){
+        return this.pFirst == null;
+    }
+    
+    //vaciar lista
+    public void VaciarLista(){
+      this.pFirst = null;
+      this.pLast = null;
+      this.size = 0;   
+    }
+    
+    //Primero de la lista
+    public Nodo Primero(){
+        return pFirst;
+    }
+    //
+    public Nodo Ultimo(){
+        return pLast;
+    }
+    
+    //Metodo para obtener el valor de un nodo en una determinada posición
+    public Object AccederAlValor(int posicion){
+        if(posicion>=0 && posicion<size){
+            if (posicion == 0) {
+                return pFirst.getDato();
+            }else{
+                Nodo aux = pFirst;
+                for (int i = 0; i < posicion; i++) {
+                    aux = aux.getpNext();
+                }
+                return aux.getDato();
+            }
+        }
+        return null;
+    }
+    
+    //insertar al inicio de la lista
+    public Nodo InsertarInicio(Object dato){
+        Nodo nuevo = new Nodo(dato);
+        if (EsVacio()){//si la lista esta vacia
+            pFirst = nuevo;
+            pLast = nuevo;
+        }else{
+            nuevo.setpNext(pFirst);//el apuntador de nuevo nodo va a apuntar a lo mismo que apunta pfirst(el primero de la list)
+            pFirst = nuevo;
+        }size ++;
+        return nuevo;
+    }
+    
+    
+    //inseratar al final de la lista
+    public Nodo Insertar(Object dato){
+        Nodo nuevo = new Nodo(dato);
+        if (EsVacio()){
+            pFirst = nuevo;
+            pLast = nuevo; 
+        }else{
+            Nodo aux = pFirst;
+            while (aux.getpNext() != null){
+                aux = aux.getpNext();
+            }aux.setpNext(nuevo);
+        }size++;
+        return nuevo;   
+    }
+
+    //Funcion para eliminar al inicio
+    public void EliminarAlInicio(){
+        if (!EsVacio()){
+            pFirst = pFirst.getpNext();
+            size--;
+        }
+    }
+    
+    //funcion para imprimir la lista
+    public void Imprimir(){
+        if (!EsVacio()){
+            Nodo aux = pFirst;
+            for(int i = 0; i<size; i++){
+                System.out.println(aux.getDato()+ "");
+                aux = aux.getpNext();
+            }
+        }else{
+            System.out.println("La lista esta vacia");
+        }
+    }
+    
+    //insertar un nodo con un dato en especifica posicion
+    public void InsertarPorPosicion(Object dato, int posicion){
+        Nodo nuevo = new Nodo(dato);
+        if (posicion ==0){
+           nuevo.setpNext(pFirst);
+           pFirst = nuevo;
+        }else{
+            if (posicion == size-1){
+                Nodo aux = pFirst;
+                while(aux.getpNext() != null){
+                    aux = aux.getpNext();
+                }
+                aux.setpNext(nuevo);
+            }else{
+                Nodo aux = pFirst;
+                for(int i = 0; i < posicion-1; i++){
+                    aux.getpNext();
+                }
+                Nodo siguiente = aux.getpNext();
+                aux.setpNext(nuevo);
+                nuevo.setpNext(siguiente);
+            }
+        }size++;
+    }
+    
+    
+    // Funcion para buscar un elemento en la lista, si lo encuentra es true
+    public boolean buscar(Object referencia){
+        Nodo aux = pFirst;
+        boolean encontrado = false;
+        while(aux != null && encontrado != true){
+            if (referencia == aux.getDato()){ 
+                encontrado = true;
+            }
+            else{
+                aux = aux.getpNext();
+            }
+        }
+        return encontrado;
+    }
+    
+    //Consulta la posición de un elemento en la lista
+    public int Posicion(Object referencia){
+        if (buscar(referencia)) {
+            Nodo aux = pFirst;
+            int cont = 0;
+            while(referencia != aux.getDato()){
+                cont ++;
+                aux = aux.getpNext();
+            }
+            return cont;
+        } else {
+            return -1;
+        }
+    }
+   
+    
+ // Función Próximo - devuelve el siguiente nodo usando posición, hay que usar getdato al lado
+public Nodo Proximo(int posicion) {
+    if (posicion >= 0 && posicion < size - 1) { // Validar que no sea la última posición
+        Nodo actual = pFirst;
+        for (int i = 0; i < posicion + 1; i++) { // ir a la posición siguiente
+            actual = actual.getpNext();
+        }
+        return actual;
+    } else {
+        return null; // Si es la última posición o posición inválida
+    }
+}
+
+// Insertar después de la posición especificada
+public void InsertarDespues(Object dato, int posicion) {
+    if (posicion >= 0 && posicion < size) {
+        Nodo pNew = new Nodo(dato);
+        
+        if (posicion == size - 1) { // Si es la última posición
+            pLast.setpNext(pNew);
+            pLast = pNew;
+            size++;
+        } else { // Si no es la última posición
+            Nodo nodoActual = pFirst;
+            for (int i = 0; i < posicion; i++) {
+                nodoActual = nodoActual.getpNext();
+            }
+            pNew.setpNext(nodoActual.getpNext());
+            nodoActual.setpNext(pNew);
+            size++;
+        }
+    }
+}
+    
+    //Metodo para editar el valor de un nodo en una posicion
+    public void editarPorPosicion(int posicion , Object dato){
+
+        if(posicion>=0 && posicion<size){
+            if(posicion == 0){
+                pFirst.setDato(dato);
+            }
+            else{
+                Nodo aux = pFirst;
+
+                for (int i = 0; i < posicion; i++) {
+                    aux = aux.getpNext();
+                }
+                aux.setDato(dato);
+            }
+        }
+    }
+    
+    //como su nombre lo indica
+    public void EliminarPorPosicion(int posicion){
+
+        if(posicion>=0 && posicion<size){
+            if(posicion == 0){
+                pFirst = pFirst.getpNext();
+            }
+            else{
+                Nodo aux = pFirst;
+                for (int i = 0; i < posicion-1; i++) {
+                    aux = aux.getpNext();
+                }
+                Nodo siguiente = aux.getpNext();
+                aux.setpNext(siguiente.getpNext());
+            }
+            size--;
+        }
+    }
+    
+    //pongo el dato, y despues lo cambio por el otro que quiero cambiar
+    public void editarPorReferencia(Object referencia, Object dato){
+        if (buscar(referencia)) {
+            Nodo aux = pFirst;
+            while(aux.getDato() != referencia){
+                aux = aux.getpNext();
+            }
+            aux.setDato(dato);
+        }
+    }
+    
+    
+    
+}
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estructura_de_datos;
+
+/**
+ *
+ * @author Diego Arreaza y Vyckhy Sarmiento
+ */
+public class Lista {
+    
+     //atributos
+    private Nodo pFirst;
+    private int size;
+    private Nodo pLast;
+    
+    //constructor
+    public Lista() {
+        this.pFirst = null;
+        this.size = 0;
+        this.pLast = null;
+    }
+    
+   
+    //getters y setters
+    public Nodo getpFirst() {
+        return pFirst;
+    }
+
+    public void setpFirst(Nodo pFirst) {
+        this.pFirst = pFirst;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Nodo getpLast() {
+        return pLast;
+    }
+
+    public void setpLast(Nodo pLast) {
+        this.pLast = pLast;
+    }
+  
+    //Es vacio
+    public boolean EsVacio(){
+        return this.pFirst == null;
+    }
+    
+    //vaciar lista
+    public void VaciarLista(){
+      this.pFirst = null;
+      this.pLast = null;
+      this.size = 0;   
+    }
+    
+    //Primero de la lista
+    public Nodo Primero(){
+        return pFirst;
+    }
+    //
+    public Nodo Ultimo(){
+        return pLast;
+    }
+    
+    //Metodo para obtener el valor de un nodo en una determinada posición
+    public Object AccederAlValor(int posicion){
+        if(posicion>=0 && posicion<size){
+            if (posicion == 0) {
+                return pFirst.getDato();
+            }else{
+                Nodo aux = pFirst;
+                for (int i = 0; i < posicion; i++) {
+                    aux = aux.getpNext();
+                }
+                return aux.getDato();
+            }
+        }
+        return null;
+    }
+    
+    //insertar al inicio de la lista
+    public Nodo InsertarInicio(Object dato){
+        Nodo nuevo = new Nodo(dato);
+        if (EsVacio()){//si la lista esta vacia
+            pFirst = nuevo;
+            pLast = nuevo;
+        }else{
+            nuevo.setpNext(pFirst);//el apuntador de nuevo nodo va a apuntar a lo mismo que apunta pfirst(el primero de la list)
+            pFirst = nuevo;
+        }size ++;
+        return nuevo;
+    }
+    
+    
+    //inseratar al final de la lista
+    public Nodo Insertar(Object dato){
+        Nodo nuevo = new Nodo(dato);
+        if (EsVacio()){
+            pFirst = nuevo;
+            pLast = nuevo; 
+        }else{
+            Nodo aux = pFirst;
+            while (aux.getpNext() != null){
+                aux = aux.getpNext();
+            }aux.setpNext(nuevo);
+        }size++;
+        return nuevo;   
+    }
+
+    //Funcion para eliminar al inicio
+    public void EliminarAlInicio(){
+        if (!EsVacio()){
+            pFirst = pFirst.getpNext();
+            size--;
+        }
+    }
+    
+    //funcion para imprimir la lista
+    public void Imprimir(){
+        if (!EsVacio()){
+            Nodo aux = pFirst;
+            for(int i = 0; i<size; i++){
+                System.out.println(aux.getDato()+ "");
+                aux = aux.getpNext();
+            }
+        }else{
+            System.out.println("La lista esta vacia");
+        }
+    }
+    
+    //insertar un nodo con un dato en especifica posicion
+    public void InsertarPorPosicion(Object dato, int posicion){
+        Nodo nuevo = new Nodo(dato);
+        if (posicion ==0){
+           nuevo.setpNext(pFirst);
+           pFirst = nuevo;
+        }else{
+            if (posicion == size-1){
+                Nodo aux = pFirst;
+                while(aux.getpNext() != null){
+                    aux = aux.getpNext();
+                }
+                aux.setpNext(nuevo);
+            }else{
+                Nodo aux = pFirst;
+                for(int i = 0; i < posicion-1; i++){
+                    aux.getpNext();
+                }
+                Nodo siguiente = aux.getpNext();
+                aux.setpNext(nuevo);
+                nuevo.setpNext(siguiente);
+            }
+        }size++;
+    }
+    
+    
+    // Funcion para buscar un elemento en la lista, si lo encuentra es true
+    public boolean buscar(Object referencia){
+        Nodo aux = pFirst;
+        boolean encontrado = false;
+        while(aux != null && encontrado != true){
+            if (referencia == aux.getDato()){ 
+                encontrado = true;
+            }
+            else{
+                aux = aux.getpNext();
+            }
+        }
+        return encontrado;
+    }
+    
+    //Consulta la posición de un elemento en la lista
+    public int Posicion(Object referencia){
+        if (buscar(referencia)) {
+            Nodo aux = pFirst;
+            int cont = 0;
+            while(referencia != aux.getDato()){
+                cont ++;
+                aux = aux.getpNext();
+            }
+            return cont;
+        } else {
+            return -1;
+        }
+    }
+   
+    
+ // Función Próximo - devuelve el siguiente nodo usando posición, hay que usar getdato al lado
+public Nodo Proximo(int posicion) {
+    if (posicion >= 0 && posicion < size - 1) { // Validar que no sea la última posición
+        Nodo actual = pFirst;
+        for (int i = 0; i < posicion + 1; i++) { // ir a la posición siguiente
+            actual = actual.getpNext();
+        }
+        return actual;
+    } else {
+        return null; // Si es la última posición o posición inválida
+    }
+}
+
+// Insertar después de la posición especificada
+public void InsertarDespues(Object dato, int posicion) {
+    if (posicion >= 0 && posicion < size) {
+        Nodo pNew = new Nodo(dato);
+        
+        if (posicion == size - 1) { // Si es la última posición
+            pLast.setpNext(pNew);
+            pLast = pNew;
+            size++;
+        } else { // Si no es la última posición
+            Nodo nodoActual = pFirst;
+            for (int i = 0; i < posicion; i++) {
+                nodoActual = nodoActual.getpNext();
+            }
+            pNew.setpNext(nodoActual.getpNext());
+            nodoActual.setpNext(pNew);
+            size++;
+        }
+    }
+}
+    
+    //Metodo para editar el valor de un nodo en una posicion
+    public void editarPorPosicion(int posicion , Object dato){
+
+        if(posicion>=0 && posicion<size){
+            if(posicion == 0){
+                pFirst.setDato(dato);
+            }
+            else{
+                Nodo aux = pFirst;
+
+                for (int i = 0; i < posicion; i++) {
+                    aux = aux.getpNext();
+                }
+                aux.setDato(dato);
+            }
+        }
+    }
+    
+    //como su nombre lo indica
+    public void EliminarPorPosicion(int posicion){
+
+        if(posicion>=0 && posicion<size){
+            if(posicion == 0){
+                pFirst = pFirst.getpNext();
+            }
+            else{
+                Nodo aux = pFirst;
+                for (int i = 0; i < posicion-1; i++) {
+                    aux = aux.getpNext();
+                }
+                Nodo siguiente = aux.getpNext();
+                aux.setpNext(siguiente.getpNext());
+            }
+            size--;
+        }
+    }
+    
+    //pongo el dato, y despues lo cambio por el otro que quiero cambiar
+    public void editarPorReferencia(Object referencia, Object dato){
+        if (buscar(referencia)) {
+            Nodo aux = pFirst;
+            while(aux.getDato() != referencia){
+                aux = aux.getpNext();
+            }
+            aux.setDato(dato);
+        }
+    }
+    
+    
+    
+}
+>>>>>>> d31299a0c6942248c37f2039a513854424c2d4e3
---
+++ src/Estructura_de_datos/Nodo.java
@@ -0,0 +1,197 @@
+<<<<<<< HEAD
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estructura_de_datos;
+
+/**
+ *hola
+ * @author Diego Arreaza y Vyckhy Sarmiento
+ */
+public class Nodo {
+  
+    //atributos para la lista
+    private Object dato;
+    private Nodo pNext;
+    
+    //atributos para el arbol
+    private patronADN data;
+    private Nodo izq;
+    private Nodo der;
+    private int frecuencia;
+    private String[] ub = new String[100];
+    
+    
+    //constructor para el arbol
+    public Nodo(patronADN data, int ub) {
+        this.data = data;
+        this.izq = null;
+        this.der = null;
+        this.frecuencia = 1;
+        int aux = 0;
+        while(this.ub[aux]!= null){
+            ub++;
+        }
+        this.ub[aux] = Integer.toString(ub);
+        
+    }
+    
+    //gettters y setters para el arbol
+
+    public patronADN getData() {
+        return data;
+    }
+
+    public void setData(patronADN data) {
+        this.data = data;
+    }
+
+    public Nodo getIzq() {
+        return izq;
+    }
+
+    public void setIzq(Nodo izq) {
+        this.izq = izq;
+    }
+
+    public Nodo getDer() {
+        return der;
+    }
+
+    public void setDer(Nodo der) {
+        this.der = der;
+    }
+    
+    
+    
+    //constructor vacio
+    public Nodo() {
+        this.dato = null;
+        this.pNext = null;
+    }
+
+    public Nodo(Object dato) {
+        this.dato = dato;
+        this.pNext = null;
+    }
+
+    public Object getDato() {
+        return dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getpNext() {
+        return pNext;
+    }
+
+    public void setpNext(Nodo pNext) {
+        this.pNext = pNext;
+    }
+    
+    
+    
+    
+}
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estructura_de_datos;
+
+/**
+ *hola
+ * @author Diego Arreaza y Vyckhy Sarmiento
+ */
+public class Nodo {
+  
+    //atributos para la lista
+    private Object dato;
+    private Nodo pNext;
+    
+    //atributos para el arbol
+    private patronADN data;
+    private Nodo izq;
+    private Nodo der;
+    private int frecuencia;
+    private String[] ub = new String[100];
+    
+    
+    //constructor para el arbol
+    public Nodo(patronADN data, int ub) {
+        this.data = data;
+        this.izq = null;
+        this.der = null;
+        this.frecuencia = 1;
+        int aux = 0;
+        while(this.ub[aux]!= null){
+            ub++;
+        }
+        this.ub[aux] = Integer.toString(ub);
+        
+    }
+    
+    //gettters y setters para el arbol
+
+    public patronADN getData() {
+        return data;
+    }
+
+    public void setData(patronADN data) {
+        this.data = data;
+    }
+
+    public Nodo getIzq() {
+        return izq;
+    }
+
+    public void setIzq(Nodo izq) {
+        this.izq = izq;
+    }
+
+    public Nodo getDer() {
+        return der;
+    }
+
+    public void setDer(Nodo der) {
+        this.der = der;
+    }
+    
+    
+    
+    //constructor vacio
+    public Nodo() {
+        this.dato = null;
+        this.pNext = null;
+    }
+
+    public Nodo(Object dato) {
+        this.dato = dato;
+        this.pNext = null;
+    }
+
+    public Object getDato() {
+        return dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getpNext() {
+        return pNext;
+    }
+
+    public void setpNext(Nodo pNext) {
+        this.pNext = pNext;
+    }
+    
+    
+    
+    
+}
+>>>>>>> d31299a0c6942248c37f2039a513854424c2d4e3
