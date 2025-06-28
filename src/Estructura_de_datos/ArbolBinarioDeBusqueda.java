11d793099f3f2deab7658a29b6815e74ce6376ba
diego arreaza <diego.arreaza@correo.unimet.edu.ve>
2025-06-28 19:37:42 -0400

    Update ArbolBinarioDeBusqueda.java

--- src/Estructura_de_datos/ArbolBinarioDeBusqueda.java
+++ src/Estructura_de_datos/ArbolBinarioDeBusqueda.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -43,57 +43,4 @@ public class ArbolBinarioDeBusqueda {
     public void Vaciar(){
         this.root = null;
     }
-    
-    
-    
 }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Estructura_de_datos;
-
-/**
- *
- * @author vyckh
- */
-public class ArbolBinarioDeBusqueda {
-    //atributos
-    private Nodo root;
-    private int size;
-    
-    //constructor
-    public ArbolBinarioDeBusqueda(Nodo root) {
-        this.root = null;
-        this.size = 0;
-    }
-
-    public Nodo getRoot() {
-        return root;
-    }
-
-    public void setRoot(Nodo root) {
-        this.root = root;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-    
-     public boolean EsVacio(){
-        return this.root == null;
-    }
-    
-    public void Vaciar(){
-        this.root = null;
-    }
-    
-    
-    
-}
->>>>>>> d31299a0c6942248c37f2039a513854424c2d4e3
